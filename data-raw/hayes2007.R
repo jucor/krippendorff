1207b83b880fb01c188e0c13d184ad4a40dab69d
Julien Cornebise <julien@cornebise.com>
2017-01-31 13:53:21 +0000

    Import Krippendorff 2004 Section 11.3.1 example data

--- data-raw/hayes2007.R
+++ data-raw/hayes2007.R
@@ -1,3 +1,3 @@
 # Manually extracted from http://afhayes.com/public/cmm2007.pdf , Table 1, p. 84
 hayes2007 <- read.csv('data-raw/hayes2007table1.csv', na.strings=".", header=TRUE)
-save(hayes2007, file='data/hayes2007.rdata')
+save(hayes2007, file='data/hayes2007.rda', compress="bzip2")
