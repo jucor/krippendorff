c512bd4cc52f75d9a542d38f5d16c494dadfad99
Julien Cornebise <julien@cornebise.com>
2017-02-01 16:00:09 +0100

    WIP

--- R/kalpha.R
+++ R/kalpha.R
@@ -1,3 +1,45 @@
-kalpha <- function(data, judges, level) {
-   0.095
+#' Turn an observation data.frame into a long data.table
+#'
+#' @param data `data.table` containing the reliability data in wide format
+#' @param unit Name of the column containing the unit ID
+#' @param observers List of names of the columns containing the oberverments, one per observer
+#'
+#' @return A long-form melted data.table with the same unit column and two new columns "observer" and "measurement"
+#'     and as many columns as units.
+to.long <- function(data, unit, observers) {
+  data.table::melt(data, id.vars = unit,
+                   measure.vars = observers,
+                   value.name = "measurement",
+                   variable.name = "observer")
+}
+
+#' Compute Krippendorff's Alpha
+#'
+#' This function implements the computation of  Krippendorff's Alpha as per
+#' http://web.asc.upenn.edu/usr/krippendorff/mwebreliability5.pdf
+#'
+#' It is designed to be space efficient for sparse oberverments, and as thus does
+#' not take as input a reliability matrix, but a long-format data.table
+#' TODO(jucor): cite properly
+#'
+#' @param data `data.table` containing the reliability data in long format
+#' @param unit Name of the column containing the unit ID
+#' @param measurement Name of the column containing the measurements, one per judge
+#' @param level c('nominal', 'ordinal'): type of oberverment data.
+kalpha <- function(data, unit, measurement, level) {
+  #  data is a data.table
+  # TODO(jucor): add default 'nominal'
+  stopifnot(level == 'nominal')
+
+  setkeyv(data, c(unit, measurement))
+
+  values.by.unit <- data[,.N, by=c(unit, measurement)]
+  print(values.by.unit)
+  values.by.unit.matrix <- as.matrix(dcast(values.by.unit,
+                                           paste(unit, "~", measurement),
+                                           value.var = "N",
+                                           fill = 0)[, -"Article"])
+  print(values.by.unit.matrix)
+
+  values.by.unit.matrix
 }
--- tests/testthat/test-binary.R
+++ tests/testthat/test-binary.R
@@ -1,8 +1,40 @@
+library(data.table)
+
 context("K-alpha without bootstrapping")
 
+test_that("Melt works as expected",
+        {
+          expected = read.csv(text='Article,observer,measurement
+1,Jon,1
+2,Jon,1
+3,Jon,0
+4,Jon,0
+5,Jon,0
+6,Jon,0
+7,Jon,0
+8,Jon,0
+9,Jon,0
+10,Jon,0
+1,Han,0
+2,Han,1
+3,Han,1
+4,Han,0
+5,Han,0
+6,Han,1
+7,Han,0
+8,Han,1
+9,Han,0
+10,Han,0', stringsAsFactors = FALSE)
+          obtained = to.long(krippendorff2004.binary.two.observers, unit="Article", observers=c("Jon", "Han"))
+          obtained$observer <- as.character(obtained$observer)
+          expect_equal(obtained, expected)
+          })
+
 test_that("K-alpha on book section 11.3.1", {
-          expect_equal(kalpha(krippendorff2004.binary.two.observers,
-                             judges = list("Jon", "Han"),
-                             level = "binary"),
+          long <- data.table(to.long(krippendorff2004.binary.two.observers, unit="Article", observers=c("Jon", "Han")))
+          expect_equal(kalpha(long,
+                              unit = "Article",
+                              measurement = "measurement",
+                              level = "nominal"),
                        0.095)
           })
