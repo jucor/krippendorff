bed3cd08a001f0a70c589cdff764475e50c43239
Julien Cornebise <julien@cornebise.com>
2017-04-07 23:58:20 +0100

    Fix wrong test

--- tests/testthat/test-boot.R
+++ tests/testthat/test-boot.R
@@ -15,7 +15,7 @@ test_that("Bootstrap on nominal data", {
   expect_equal(length(result$samples), nboot)
   expect_gt(result$ll95, 0)
   expect_gt(result$ul95, 0)
-  expect_gt(result$ul95, result$ll95)
+  expect_gte(result$ul95, result$ll95)
   # TODO(jucor): find a nominal dataset to bootstrap for which Krippendorff gives values that I can use as test
   # TODO(jucor): or implement ordinal level to use the values in Hayes 2007
 })
@@ -52,7 +52,7 @@ test_that("Bootstrap with only two observers", {
   expect_equal(length(result$samples), nboot)
   expect_gt(result$ll95, 0)
   expect_gt(result$ul95, 0)
-  expect_gt(result$ul95, result$ll95)
+  expect_gte(result$ul95, result$ll95)
   # TODO(jucor): find a nominal dataset to bootstrap for which Krippendorff gives values that I can use as test
   # TODO(jucor): or implement ordinal level to use the values in Hayes 2007
 })
